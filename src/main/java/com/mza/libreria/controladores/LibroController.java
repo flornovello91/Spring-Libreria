15eecfab681365aafc4e47163e3c83a7d0cb0f37
Florencia Novello <flornovello91@gmail.com>
2021-11-05 11:12:47 -0300

    developing

--- src/main/java/com/mza/libreria/controladores/LibroController.java
+++ src/main/java/com/mza/libreria/controladores/LibroController.java
@@ -1,5 +1,35 @@
 package com.mza.libreria.controladores;
 
+import com.mza.libreria.MyException.MyException;
+import com.mza.libreria.servicios.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/libro")  //localhost:8080/libro
 public class LibroController {
     
+    @Autowired
+    private LibroService libroService;
+    
+    @GetMapping("registro")       //localhost:8080/perro/registro
+    public String formularioIngreso(){
+        return ("formularioIngreso");
+    }
+    
+    @PostMapping("/registro")
+    public String guardarLibro(@RequestParam Integer isbn,@RequestParam String titulo,@RequestParam String Autor,@RequestParam String Editorial,@RequestParam Integer anio) throws MyException{
+        
+        try{
+            libroService.guardarLibro(isbn,titulo,Autor,Editorial,anio);
+            return ("formularioIngreso");
+        }catch (MyException e){
+            //poner el model aca
+            return ("formularioIngreso");
+        }
+    }
 }
--- src/main/java/com/mza/libreria/controladores/MainController.java
+++ src/main/java/com/mza/libreria/controladores/MainController.java
@@ -1,5 +1,20 @@
 package com.mza.libreria.controladores;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+//ESTO INDICA QUE LA CLASE ES UN CONTROLER
+@Controller 
+
+//ESTO LO QUE NOS HACE ES MOSTRARNOS EL CAMINO QUE SEGUIRÁ LA URL PARA ESTA VISTA Y TODO LAS ACCIONES LLEVADAS 
+//A CABO EN LA MISMA. EN ESTE CASO 
+@RequestMapping("/")  
+
+public class MainController {
+    
+    @GetMapping("/")              //cuando el URL este en localhost:8080/ se va a mostrar mi pagina principal
+    public String index(){
+        return "index";
+    }
 }
